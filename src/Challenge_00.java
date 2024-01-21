4adb494855600fb12139bbb549fa701048061569
Muhammad Azeemuddin <muhammad.azeemuddin@hotmail.com>
2024-01-21 18:56:54 +0500

    pc add rename files

--- src/Challenge_00.java
+++
@@ -1,7 +0,0 @@
-class Challenge_00 {
-    public static void main(String[] args) {
-        System.out.print("Good Morning");
-        System.out.println("Subscribe");
-        System.out.println("hi");
-    }
-}
