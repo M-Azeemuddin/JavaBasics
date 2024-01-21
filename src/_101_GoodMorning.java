65b8921c3e5d77d9e6bb7a725f0a8317d2111fc2
Muhammad Azeemuddin <muhammad.azeemuddin@hotmail.com>
2024-01-21 18:50:45 +0500

    pc add _101_GoodMorning

---
+++ src/_101_GoodMorning.java
@@ -0,0 +1,8 @@
+public class _101_GoodMorning {
+    public static void main(String[] args) {
+        System.out.print("Good Morning \n");
+        System.out.print("Subscribe\n");
+        System.out.print("hi");
+    }
+}
+
